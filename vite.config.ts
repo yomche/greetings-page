d4e94f1f20f0dff6d491f806ab903574f5081e07
Ekaterina Dorozhkina <katerina.smirnova98@yandex.ru>
2023-11-13 14:00:27 +0300

    Add gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/greetings-page/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -14,7 +15,6 @@ export default defineConfig({
       "@constants": path.resolve(__dirname, "./src/constants"),
     },
   },
-  base: "/",
   envDir: "./env",
   server: {
     host: "127.0.0.1",
